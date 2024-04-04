1e70c934c7a886a4013ee91cca60126c7cc56b07
Guilherme Selair <guilherme.lima@getrak.com.br>
2024-04-04 08:58:04 -0300

    feat: added GTM configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import type { Metadata } from "next";
+import ScriptTag from "next/script";
 
 import { cn } from "@/helpers/utils";
 import { fonts } from "@/constants/fonts";
@@ -19,6 +20,20 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="pt-BR">
+			<head>
+				<ScriptTag
+					id="Google Tag Manager-Script"
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-P2N7RCLP');
+						`,
+					}}
+				/>
+			</head>
 			<body
 				className={cn(
 					"min-h-screen bg-background font-sans antialiased",
@@ -27,6 +42,16 @@ export default function RootLayout({
 					fonts.inter.variable
 				)}
 			>
+				{/* Google Tag Manager */}
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-P2N7RCLP"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+						title="Google Tag Manager"
+					></iframe>
+				</noscript>
 				<div
 					className="
 						min-h-screen
