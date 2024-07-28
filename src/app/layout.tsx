7c73ba565cafde9829fb2e51d70d797b312d779f
Guilherme Selair <guilherme.selair@gmail.com>
2024-07-28 12:56:44 -0300

    feat: added adsense id account

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 		<html lang="pt-BR">
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+				<meta name="google-adsense-account" content="ca-pub-3489651549797034"/>
 				<ScriptTag
 					id="Google Tag Manager-Script"
 					dangerouslySetInnerHTML={{
